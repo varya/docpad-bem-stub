85cbca42e4605daf89b765001b3c995d2cd3797a
Varya Stepanova <mail@varya.me>
2014-05-28 19:53:47 +0400

    Menu CSS

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -3,11 +3,36 @@
     title: 'Article',
     favicon: '/favicon.ico',
     head: [
+        { elem: 'css', url: 'http://fonts.googleapis.com/css?family=Arvo', ie: false },
         { elem: 'css', url: 'index.min.css', ie: false },
         { elem: 'meta', attrs: { name: 'description', content: '' }},
         { elem: 'meta', attrs: { name: 'keywords', content: '' }}
     ],
     content:[
+        {
+            block: 'site-menu',
+            content: [
+                {
+                    elem: 'item',
+                    mods: {
+                        current: true
+                    },
+                    content: {
+                        block: 'link',
+                        url: '#',
+                        content: 'Index'
+                    }
+                },
+                {
+                    elem: 'item',
+                    content: {
+                        block: 'link',
+                        url: '#',
+                        content: 'About'
+                    }
+                }
+            ]
+        },
         {
             block: 'posts-list'
         },
